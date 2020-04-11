daf5eb67619030983b0c3a133e4501f336013060
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-04-11 19:48:36 +0200

    Updated linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,11 @@ module.exports = {
   ],
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
   ],
+  rules: {
+    'react/prop-types': 'off'
+  }
 };
