d22a041cb7443ad0cb29787e66a22ee7aaa10e60
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-04-13 18:36:41 +0200

    Reorganized mock data

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,33 +1,7 @@
 import React, { FunctionComponent } from 'react';
-import { Card, CardProps } from '../card/card';
 
-const businesses: CardProps[] = [
-  {
-    name: 'Da Gino',
-    description: 'Famoso ristorante italiano',
-    address: 'Derde Oosterparkstraat 201, 1092 CZ Amsterdam'
-  },
-  {
-    name: 'Mediterraneo',
-    description: 'Excellent italian cuisine',
-    address: 'Derde Oosterparkstraat 202, 1092 CZ Amsterdam'
-  },
-  {
-    name: 'Shibuya Sushi',
-    description: 'Sushi bar',
-    address: 'Derde Oosterparkstraat 203, 1092 CZ Amsterdam'
-  },
-  {
-    name: 'Mario\'s café',
-    description: 'Best coffee in town',
-    address: 'Derde Oosterparkstraat 204, 1092 CZ Amsterdam'
-  },
-  {
-    name: 'Gusto Giusto',
-    description: 'Italian gelato parlor',
-    address: 'Derde Oosterparkstraat 205, 1092 CZ Amsterdam'
-  }
-];
+import { Card } from '../card/card';
+import { businesses } from './fixtures/mockdata';
 
 export interface ListProps {
   itemsGroupSize?: number;
