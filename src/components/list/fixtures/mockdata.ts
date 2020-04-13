d8eb0dd6d757f7aaaf5a87b7f6938968e5470705
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-04-13 23:16:19 +0200

    Updated mock data

--- src/components/list/fixtures/mockdata.ts
+++ src/components/list/fixtures/mockdata.ts
@@ -25,5 +25,105 @@ export const businesses: CardProps[] = [
     name: 'Gusto Giusto',
     description: 'Italian gelato parlor',
     address: 'Derde Oosterparkstraat 205, 1092 CZ Amsterdam'
-  }
+  },
+  {
+    name: 'Da Gino',
+    description: 'Famoso ristorante italiano',
+    address: 'Derde Oosterparkstraat 201, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Mediterraneo',
+    description: 'Excellent italian cuisine',
+    address: 'Derde Oosterparkstraat 202, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Shibuya Sushi',
+    description: 'Sushi bar',
+    address: 'Derde Oosterparkstraat 203, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Mario\'s café',
+    description: 'Best coffee in town',
+    address: 'Derde Oosterparkstraat 204, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Gusto Giusto',
+    description: 'Italian gelato parlor',
+    address: 'Derde Oosterparkstraat 205, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Da Gino',
+    description: 'Famoso ristorante italiano',
+    address: 'Derde Oosterparkstraat 201, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Mediterraneo',
+    description: 'Excellent italian cuisine',
+    address: 'Derde Oosterparkstraat 202, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Shibuya Sushi',
+    description: 'Sushi bar',
+    address: 'Derde Oosterparkstraat 203, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Mario\'s café',
+    description: 'Best coffee in town',
+    address: 'Derde Oosterparkstraat 204, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Gusto Giusto',
+    description: 'Italian gelato parlor',
+    address: 'Derde Oosterparkstraat 205, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Da Gino',
+    description: 'Famoso ristorante italiano',
+    address: 'Derde Oosterparkstraat 201, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Mediterraneo',
+    description: 'Excellent italian cuisine',
+    address: 'Derde Oosterparkstraat 202, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Shibuya Sushi',
+    description: 'Sushi bar',
+    address: 'Derde Oosterparkstraat 203, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Mario\'s café',
+    description: 'Best coffee in town',
+    address: 'Derde Oosterparkstraat 204, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Gusto Giusto',
+    description: 'Italian gelato parlor',
+    address: 'Derde Oosterparkstraat 205, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Da Gino',
+    description: 'Famoso ristorante italiano',
+    address: 'Derde Oosterparkstraat 201, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Mediterraneo',
+    description: 'Excellent italian cuisine',
+    address: 'Derde Oosterparkstraat 202, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Shibuya Sushi',
+    description: 'Sushi bar',
+    address: 'Derde Oosterparkstraat 203, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Mario\'s café',
+    description: 'Best coffee in town',
+    address: 'Derde Oosterparkstraat 204, 1092 CZ Amsterdam'
+  },
+  {
+    name: 'Gusto Giusto',
+    description: 'Italian gelato parlor',
+    address: 'Derde Oosterparkstraat 205, 1092 CZ Amsterdam'
+  },
 ];
